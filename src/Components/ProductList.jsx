90aa78e487130587f4e524a13f225d9597f9963c
Relix18 <rellix049@gmail.com>
2023-10-31 13:42:24 +0530

    api fetch

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "../styles/ProductList.scss";
+import data from "../data.js";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div id="productList">
+      {data.map((item) => (
+        <div className="card">
+          <img src={item.thumbnail} alt="product" />
+          <h5>{item.title}</h5>
+          <h3>₹{item.price}</h3>
+          <button>ADD TO CART</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
