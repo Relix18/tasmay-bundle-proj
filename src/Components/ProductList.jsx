e922a782c30afc67fd58ce6fcac70d55083d03b9
Relix18 <rellix049@gmail.com>
2023-11-06 12:18:08 +0530

    Pagination

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ProductList.scss";
-import data from "../data.js";
+import data from "../data";
+import Pagination from "./Pagination";
 
 const ProductList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const postPerPage = 10;
+  const lastIndex = currentPage * postPerPage;
+  const firstIndex = lastIndex - postPerPage;
+  const currentPost = data.slice(firstIndex, lastIndex);
+
   return (
-    <div id="productList">
-      {data.map((item) => (
-        <div className="card">
-          <img src={item.thumbnail} alt="product" />
-          <h5>{item.title}</h5>
-          <h3>₹{item.price}</h3>
-          <button>ADD TO CART</button>
-        </div>
-      ))}
+    <div>
+      <div id="productList">
+        {currentPost.map((item) => (
+          <div className="card">
+            <img src={item.thumbnail} alt="product" />
+            <h5>{item.title}</h5>
+            <h3>${item.price}</h3>
+            <button>ADD TO CART</button>
+          </div>
+        ))}
+      </div>
+      <Pagination
+        totalPosts={data.length}
+        postPerPage={postPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
--- src/Components/pagination.jsx
+++ src/Components/pagination.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "../styles/Pagination.scss";
 
-const pagination = () => {
-  return <div>pagination</div>;
+const Pagination = ({
+  totalPosts,
+  postPerPage,
+  setCurrentPage,
+  currentPage,
+}) => {
+  let pages = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
+    pages.push(i);
+  }
+
+  const isPrev = currentPage > 1;
+  const isNext = currentPage < pages.length;
+
+  return (
+    <div id="Pagination">
+      <button
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={!isPrev}
+        className="btn"
+      >
+        Prev
+      </button>
+      {pages.map((page, index) => (
+        <button
+          className={`${currentPage === page ? "active" : ""}`}
+          key={index}
+          onClick={() => setCurrentPage(page)}
+        >
+          {page}
+        </button>
+      ))}
+      <button
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={!isNext}
+        className="btn"
+      >
+        Next
+      </button>
+    </div>
+  );
 };
 
-export default pagination;
+export default Pagination;
